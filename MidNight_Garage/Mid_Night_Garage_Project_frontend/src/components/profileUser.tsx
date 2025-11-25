bc02d6f6c4b6a2010600d93590f48eea7e79353d
Bruno <brunofirmino960@gmail.com>
2025-11-25 16:36:40 -0300

    terminando estilização2

--- MidNight_Garage/Mid_Night_Garage_Project_frontend/src/components/profileUser.tsx
+++ MidNight_Garage/Mid_Night_Garage_Project_frontend/src/components/profileUser.tsx
@@ -232,8 +232,6 @@ export default function Profile() {
                               day: "2-digit",
                               month: "2-digit",
                               year: "numeric",
-                              hour: "2-digit",
-                              minute: "2-digit"
                             })}
                           </div>
                           {pedido.usuario_telefone && (
